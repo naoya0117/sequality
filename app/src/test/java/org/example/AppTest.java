63998bf839e7a12a6528c07d10bda0baef86d0ce
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-07-01 10:29:59 +0900

    feat: 失敗するテストパターン

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -6,9 +6,27 @@ package org.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+/**
+ * Appクラスのテスト
+ */
 public class AppTest {
-    @Test public void appHasAGreeting() {
+    /**
+     * getGreeting関数のテスト。
+     * {@link App#getGreeting()} メソッNotド何らかの値を返すことを確認.
+     */
+    @Test
+    public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    /**
+     * getGreeting関数のテスト。
+     * {@link App#getGreeting()} メソッドが "Hello World" を返すことを確認。
+     */
+    @Test
+    public void testAppReturnHelloWorld() {
+        App classUnderTest = new App();
+        assertEquals("Hello World!", classUnderTest.getGreeting());
     }
 }
